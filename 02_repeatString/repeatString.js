6f639949cc590a28223dcf12dd4394ea0b1ad9f8
Jordan Keller <keller.jc@gmail.com>
2022-08-22 08:45:52 -0700

    Complete JS lesson 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
 
-};
+
+function repeatString(str, num) {
+
+    for (let i = num; i <= num; i++) {
+        let returnedString = "";
+        if (num < 0) {
+            return 'ERROR'
+
+        } else if (num === 0) {
+            return ''
+
+        } else {
+            returnedString += str.repeat(num)
+            return returnedString
+
+        }
+
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
