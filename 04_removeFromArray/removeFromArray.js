d53a736b44f69fe9e2697601c7c1fa6c87d4dc77
Jordan Keller <keller.jc@gmail.com>
2022-09-08 13:05:00 -0700

    Complete JS lesson 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...items) {
+    for (let item of items) {
+        let arrayIndex = array.indexOf(item);
+        if (arrayIndex >= 0) {
+            array.splice(arrayIndex, 1);
+        }
+
+    }
+    return array
 
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
