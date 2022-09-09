61e3af7272c4b6fdd8e9d51034c4de1d63509302
Jordan Keller <keller.jc@gmail.com>
2022-09-09 11:48:24 -0700

    Complete JS lesson 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+
+
+const ftoc = function (deg) {
+  // C = (32°F − 32) × 5/9 = 0°C
+  let degC = (deg - 32) * (5 / 9)
+  let roundedDegC = Math.round(degC * 10) / 10
+  return roundedDegC
 
 };
 
-const ctof = function() {
-
+const ctof = function (deg) {
+  // F = (0°C × 9/5) + 32 = 32°F
+  let degF = (deg * (9 / 5) + 32)
+  let roundedDegF = Math.round(degF * 10) / 10
+  return roundedDegF
 };
 
 // Do not edit below this line
