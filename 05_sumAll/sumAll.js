0fd81a980670d7af54dcd449448a2f65153c116b
Jordan Keller <keller.jc@gmail.com>
2022-09-09 11:06:04 -0700

    Complete JS lesson 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+// total numbers -> (y-x)+1
+// sum -> n(n+1)/2
+
+const sumAll = function (x, y) {
+
+    if (Number(x) < 0 || Number(y) < 0) {
+        return 'ERROR'
+    } else if (x <= y && Number.isInteger(x) && Number.isInteger(y)) {
+
+        let n = (y - x) + 1
+        let sumAll = n * (n + 1) / 2
+
+        return sumAll
+    } else if (x > y && Number.isInteger(x) && Number.isInteger(y)) {
+        let n = (x - y) + 1
+        let sumAll = n * (n + 1) / 2
+
+        return sumAll
+    } else {
+        return 'ERROR'
+    }
 
 };
 
