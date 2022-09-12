6966b0e6a38cb893c107d54a43d89e99bd036eb0
Jordan Keller <keller.jc@gmail.com>
2022-09-12 11:46:54 -0700

    Complete JS lesson 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function (x, y) {
+  let addNumbers = x + y
+  return addNumbers
 
 };
 
-const power = function() {
-	
+const subtract = function (x, y) {
+  let subtractNumbers = x - y
+  return subtractNumbers
+
 };
 
-const factorial = function() {
-	
+const sum = function ([...nums]) {
+  let sumNumbers = 0
+  for (let num of nums) {
+    sumNumbers += Number(num)
+  }
+  return sumNumbers
+
+};
+
+const multiply = function ([...nums]) {
+  let multNumbers = 1
+  for (let num of nums) {
+    multNumbers *= Number(num)
+  }
+  return multNumbers
+
+
+};
+
+const power = function (num, power) {
+  let powerNumber = Number(num) ** Number(power)
+  return powerNumber
+
+};
+
+const factorial = function (num) {
+  if (num === Number(0) || num === Number(1)) {
+    return Number(1)
+  } else if (num > 1) {
+    for (let i = num - 1; i >= 1; i--) {
+      num *= i
+    }
+
+    return num
+  }
 };
 
 // Do not edit below this line
