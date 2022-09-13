126022743086fd4ca4ddd46eb5bb53d55a3931d1
Jordan Keller <keller.jc@gmail.com>
2022-09-13 12:33:13 -0700

    Complete JS lesson 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (num <= 0) {
+        return "OOPS"
+    } else {
+        let firstTerm = 1;
+        let secondTerm = 1;
+        let sum;
+        let i = 0;
+        for (i = 2; i < Number(num); i++) {
+            sum = Number(firstTerm) + Number(secondTerm);
+            firstTerm = secondTerm;
+            secondTerm = sum;
+
+        }
+        return secondTerm
+    }
+
 
 };
 
