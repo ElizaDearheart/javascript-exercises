3214827f67d16ead3ce1b6d02998b522329df984
Jordan Keller <keller.jc@gmail.com>
2022-09-13 11:13:44 -0700

    Complete JS lesson 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    //forward string
+    let punctuationless = words.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    let finalPunctuationlessString = punctuationless.replace(/\s{1,}/g, " ");
+    let lowerCaseString = finalPunctuationlessString.toLowerCase()
+    let singleForwardString = lowerCaseString.split(' ').join("");
+
+    //reverse string
+    let splitForwardString = singleForwardString.split("");
+    let reverseString = splitForwardString.reverse();
+    let reassembledBackwardsString = reverseString.join("");
+
+    if (singleForwardString === reassembledBackwardsString) {
+        return true
+    } else {
+        return false
+    }
 
 };
 
